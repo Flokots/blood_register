2a0766187a636ced6717ff9fdb7f2942171cb91c
Flokots <florencekotohoyoh@gmail.com>
2022-12-23 17:33:39 +0100

    Add readfile function

--- main.c
+++ main.c
@@ -14,61 +14,21 @@ typedef struct donor
 } donor;
 
 void list(donor *d, int l);
+int readFile(donor **d, char *fName, int *l);
 
 int main(int argc, char *argv[])
 {
     donor *donors = NULL;
     char dateString[11];
-    int length = 0, again;
+    int length = 0;
+    int again;
 
     // Get name of the file as a command line argument
     if (argc == 2)
     {
-        printf("This program helps you to register the blood donors\n\n");
-
-        char ch;
-        int i;
-        
-        FILE *fp = fopen(argv[1], "r");
-
-        if (fp == NULL)
-        {
-            perror("fopen");
-            exit(EXIT_FAILURE);
-        }
-
-        while ((ch = fgetc(fp)) != EOF)
-        {
-            if (ch == '\n')
-            {
-                length++;
-            }
-        }
-
-        if (!(donors = (donor *)malloc(length * sizeof(donor))))
-        {
-            printf("ERROR: there isn't enough memory. \n");
-            if (overAgain())
-            {
-                return 1;
-            }
-            else
-            {
-                printf("Program exits.\n");
-                return 2;
-            }
-        }
-
-        rewind(fp);
-
-        for (i = 0; i < length; i++)
-        {
-            fscanf(fp, "%s%s%s%d%s", (donors)[i].name, (donors)[i].blood_group, (donors)[i].email, &(donors)[i].donations, donors[i].last_donation_date);
-        }
-
-        fclose(fp);
-
-        printf("The data has been read into the list.\n");
+        char *fileName = NULL;
+        fileName = argv[1];
+        readFile(&donors, fileName, &length);
     }
     else if (argc > 2)
     {
@@ -104,4 +64,55 @@ void list(donor *d, int l)
     {
         printf("Name: %-20s bloodgroup: %-3s email: %-20s  donations: %d last_donation_date: %-11s\n", d[i].name, d[i].blood_group, d[i].email, d[i].donations, d[i].last_donation_date);
     }
+}
+
+int readFile(donor **d, char *fName, int *l)
+{
+     printf("This program helps you to register the blood donors\n\n");
+
+        char ch;
+        int i;
+        
+        FILE *fp = fopen(fName, "r");
+
+        if (fp == NULL)
+        {
+            perror("fopen");
+            exit(EXIT_FAILURE);
+        }
+
+        while ((ch = fgetc(fp)) != EOF)
+        {
+            if (ch == '\n')
+            {
+                (*l)++;
+            }
+        }
+
+        if (!(*d = (donor *)malloc(*l*sizeof(donor))))
+        {
+            printf("ERROR: there isn't enough memory. \n");
+            if (overAgain())
+            {
+                return 1;
+            }
+            else
+            {
+                printf("Program exits.\n");
+                return 2;
+            }
+        }
+
+        rewind(fp);
+
+        for (i = 0; i < *l; i++)
+        {
+            fscanf(fp, "%s%s%s%d%s", (*d)[i].name, (*d)[i].blood_group, (*d)[i].email, &(*d)[i].donations, (*d)[i].last_donation_date);
+        }
+
+        fclose(fp);
+
+        printf("The data has been read into the list.\n");
+
+        return 0;
 }
